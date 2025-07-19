e3fc43ee6cef901db04586bf6755b51ff7d325aa
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2025-07-18 22:02:22 -0400

    Fixed syntax

--- src/queries/initial_setup.sql
+++ src/queries/initial_setup.sql
@@ -3,12 +3,14 @@ START TRANSACTION;
 -- Entidades
 
 CREATE TABLE IF NOT EXISTS curso (
-	codigo INT  NOT NULL  PRIMARY KEY,
-	anio INT  NOT NULL,
+	codigo INT  NOT NULL,
+	anio INT  NOT NULL
 );
 
+ALTER TABLE curso ADD CONSTRAINT PK_curso PRIMARY KEY (codigo);
+
 CREATE TABLE IF NOT EXISTS alumno (
-	rut	VARCHAR(15) NOT NULL PRIMARY KEY,
+	rut	VARCHAR(15) NOT NULL,
 	nombres VARCHAR(50) NOT NULL,
 	apellido_paterno VARCHAR(20) NOT NULL,
 	apellido_materno VARCHAR(20) NOT NULL,
@@ -18,29 +20,34 @@ CREATE TABLE IF NOT EXISTS alumno (
 	codigo_curso INT  NOT NULL REFERENCES curso(codigo) ON DELETE CASCADE
 );
 
+ALTER TABLE alumno ADD CONSTRAINT PK_alumno PRIMARY KEY (rut);
 
 CREATE TABLE IF NOT EXISTS profesor (
-	rut	VARCHAR(15) NOT NULL PRIMARY KEY,
+	rut	VARCHAR(15) NOT NULL,
 	nombres VARCHAR(50) NOT NULL,
 	apellido_paterno VARCHAR(20) NOT NULL,
 	apellido_materno VARCHAR(20) NOT NULL,
 	direccion VARCHAR(50) NOT NULL,
 	ciudad VARCHAR(20) NOT NULL
 );
+
+ALTER TABLE profesor ADD CONSTRAINT PK_profesor PRIMARY KEY (rut);
 
 CREATE TABLE IF NOT EXISTS extraprogramatica (
-	codigo INT  NOT NULL  PRIMARY KEY,
+	codigo INT  NOT NULL,
 	nombre VARCHAR(20) NOT NULL,
-	dia INT(1)  NOT NULL,
+	dia INT  NOT NULL,
 	hora_inicio INT  NOT NULL,
 	hora_fin INT  NOT NULL,
-	cupos INT UNSINGED NOT NULL,
+	cupos INT  NOT NULL,
 	lugar VARCHAR(50) NOT NULL,
-	rut_profesor VARCHAR(15) NOT NULL PRIMARY KEY REFERENCES profesor(rut) ON DELETE CASCADE
+	rut_profesor VARCHAR(15) NOT NULL REFERENCES profesor(rut) ON DELETE CASCADE
 );
 
+ALTER TABLE extraprogramatica ADD CONSTRAINT PK_extraprogramatica PRIMARY KEY (codigo);
+
 CREATE TABLE IF NOT EXISTS apoderado (
-	rut	VARCHAR(15) NOT NULL PRIMARY KEY,
+	rut	VARCHAR(15) NOT NULL,
 	nombres VARCHAR(50) NOT NULL,
 	apellido_paterno VARCHAR(20) NOT NULL,
 	apellido_materno VARCHAR(20) NOT NULL,
@@ -48,48 +55,65 @@ CREATE TABLE IF NOT EXISTS apoderado (
 	ciudad VARCHAR(20) NOT NULL
 );
 
+ALTER TABLE apoderado ADD CONSTRAINT PK_apoderado PRIMARY KEY (rut);
+
 CREATE TABLE IF NOT EXISTS especialidad (
-	codigo_especialidad INT  NOT NULL PRIMARY KEY REFERENCES especialidad(codigo),
+	codigo INT  NOT NULL,
 	descripcion VARCHAR(50) NOT NULL
 );
+
+ALTER TABLE especialidad ADD CONSTRAINT PK_especialidad PRIMARY KEY (codigo);
 
 -- Relaciones
 
 CREATE TABLE IF NOT EXISTS media (
-	codigo_curso INT  NOT NULL PRIMARY KEY REFERENCES curso(codigo) ON DELETE CASCADE,
+	codigo_curso INT  NOT NULL REFERENCES curso(codigo) ON DELETE CASCADE,
 	orientacion VARCHAR(20) NOT NULL
 );
 
+ALTER TABLE media ADD CONSTRAINT PK_media PRIMARY KEY (codigo_curso);
+
 CREATE TABLE IF NOT EXISTS basica (
-	codigo_curso INT  NOT NULL PRIMARY KEY REFERENCES curso(codigo) ON DELETE CASCADE,
+	codigo_curso INT  NOT NULL REFERENCES curso(codigo) ON DELETE CASCADE
 );
+
+ALTER TABLE basica ADD CONSTRAINT PK_basica PRIMARY KEY (codigo_curso);
 
 CREATE TABLE IF NOT EXISTS es_jefe (
-	codigo_curso INT  NOT NULL PRIMARY KEY REFERENCES curso(codigo) ON DELETE CASCADE,
-	rut_profesor_jefe VARCHAR(15) NOT NULL PRIMARY KEY REFERENCES profesor(rut) ON DELETE CASCADE
+	codigo_curso INT  NOT NULL REFERENCES curso(codigo) ON DELETE CASCADE,
+	rut_profesor_jefe VARCHAR(15) NOT NULL REFERENCES profesor(rut) ON DELETE CASCADE
 );
+
+ALTER TABLE es_jefe ADD CONSTRAINT PK_es_jefe PRIMARY KEY (codigo_curso, rut_profesor_jefe);
 
 CREATE TABLE IF NOT EXISTS es_asistente (
-	codigo_curso INT  NOT NULL PRIMARY KEY REFERENCES curso(codigo) ON DELETE CASCADE,
-	rut_profesor_asistente VARCHAR(15) NOT NULL PRIMARY KEY REFERENCES profesor(rut) ON DELETE CASCADE
+	codigo_curso INT  NOT NULL REFERENCES basica(codigo_curso) ON DELETE CASCADE,
+	rut_profesor_asistente VARCHAR(15) NOT NULL REFERENCES profesor(rut) ON DELETE CASCADE
 );
+
+ALTER TABLE es_asistente ADD CONSTRAINT PK_es_asistente PRIMARY KEY (codigo_curso, rut_profesor_asistente);
 
 CREATE TABLE IF NOT EXISTS participa (
-	rut_alumno VARCHAR(15) NOT NULL PRIMARY KEY REFERENCES alumno(rut) ON DELETE CASCADE,
-	codigo INT  NOT NULL PRIMARY KEY REFERENCES extraprogramatica(codigo) ON DELETE CASCADE,
+	rut_alumno VARCHAR(15) NOT NULL REFERENCES alumno(rut) ON DELETE CASCADE,
+	codigo INT  NOT NULL REFERENCES extraprogramatica(codigo) ON DELETE CASCADE
 );
 
+ALTER TABLE participa ADD CONSTRAINT PK_participa PRIMARY KEY (rut_alumno, codigo);
+
 CREATE TABLE IF NOT EXISTS representa (
-	rut_alumno VARCHAR(15) NOT NULL PRIMARY KEY REFERENCES alumno(rut) ON DELETE CASCADE,
-	rut_apoderado VARCHAR(15) NOT NULL PRIMARY KEY REFERENCES apoderado(rut) ON DELETE CASCADE,
+	rut_alumno VARCHAR(15) NOT NULL REFERENCES alumno(rut) ON DELETE CASCADE,
+	rut_apoderado VARCHAR(15) NOT NULL REFERENCES apoderado(rut) ON DELETE CASCADE,
 	fecha_inicio DATE NOT NULL,
 	fecha_termino DATE NOT NULL
 );
 
+ALTER TABLE representa ADD CONSTRAINT PK_representa PRIMARY KEY (rut_alumno, rut_apoderado);
+
 CREATE TABLE IF NOT EXISTS tiene (
-	rut_profesor VARCHAR(15) NOT NULL PRIMARY KEY REFERENCES profesor(rut) ON DELETE CASCADE,
-	codigo_especialidad INT  NOT NULL PRIMARY KEY REFERENCES especialidad(codigo) ON DELETE CASCADE
+	rut_profesor VARCHAR(15) NOT NULL REFERENCES profesor(rut) ON DELETE CASCADE,
+	codigo_especialidad INT  NOT NULL REFERENCES especialidad(codigo) ON DELETE CASCADE
 );
 
+ALTER TABLE tiene ADD CONSTRAINT PK_tiene PRIMARY KEY (rut_profesor, codigo_especialidad);
 
 COMMIT;
