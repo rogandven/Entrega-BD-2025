a961300d6f60977d0cdcc09d621c71fa547604c4
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2025-07-19 22:05:22 -0400

    Added prototype GUI

--- src/src/src/main.java
+++ src/src/src/main.java
@@ -10,10 +10,12 @@ import src.database.Database;
 
 public class main {
     public static void main(String[] args) {
+        /*
         Database db = new Database();
         db.connect("localhost", 5432, "bdproyecto", "postgres", "secret");
         String[][] query = db.doReceivingQuery("SELECT c.codigo, c.anio FROM curso c;", 2);
         System.out.println(Arrays.deepToString(query));
         db.closeConnection();
+        */
     }
 }
